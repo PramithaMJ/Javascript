9569c5076fbed2beb37590664fcf49d373f532d1
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-08-05 18:31:03 +0530

    async await

---
+++ Async/await.js
@@ -0,0 +1,31 @@
+//Async /Await
+
+const makeApiRequest = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //resolve
+      let isFetched = true;
+      let user = {
+        name: "Miki",
+        age: 20,
+      };
+      if (isFetched) {
+        resolve(user);
+      }
+      //reject
+      reject("Error fetching user ");
+    }, 5000);
+  });
+};
+const getUser = async () => {
+ 
+  try {
+     //make api call to fetch user
+  const res = await makeApiRequest();
+  console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getUser();
